80e079ad9f3f9f450c70144e96bd0c4f9857aaca
Diego Lopes Sakata <dihsakata@gmail.com>
2025-03-31 12:50:41 -0300

    feat: criacao e teste em pilhas

---
+++ Estudos para a prova/pilha.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int *dados;
+    int topo;
+    int capacidade;
+}t_pilha;
+
+void constroi_pilha(t_pilha *p, int capacidade);
+int pilha_cheia(t_pilha *p);
+int pilha_vazia(t_pilha *p);
+int pop(t_pilha *p, int *removido);
+int push(t_pilha *p, int dado);
+void exibe_pilha(t_pilha *p);
+int pop_mais(t_pilha *p, int quantidade);
+int inverte_pilha(t_pilha *p);
