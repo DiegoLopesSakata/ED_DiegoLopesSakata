25c7f642d1ea01b4067646d35f99c1f563275ca6
Diego Lopes Sakata <dihsakata@gmail.com>
2025-06-04 11:28:48 -0300

    refactor: altera o codigo teste_abb.c

--- Estudos P2/arvore/teste_abb.c
+++ Estudos P2/arvore/teste_abb.c
@@ -6,17 +6,48 @@ int main(){
     constroi_abb(&abb);
     srand(time(0));
 
-    for(int i = 0; i < 20; i++){
-        inserir_abb(rand() % 20, &abb);
-        exibir_abb(&abb);
-    }
+    int op;
+    do{
+        printf("-------Menu-------\n\n");
+        printf("1. Adicionar n valores na arvore\n");
+        printf("2. Adicionar um valor especifico\n");
+        printf("3. Exibir arvore\n");
+        printf("4. buscar valor\n");
+        printf("0. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &op);
 
-    if(busca_abb(5, &abb)){
-        printf("\nO 5 esta na arvore!\n");
-    }
-    else{
-        printf("\nO 5 nao esta na arvore!\n");
-    }
+        switch(op){
+            case 1: {int n;
+                    printf("Quantos valores quer colocar na arvore? ");
+                    scanf("%d", &n);
+                    for(int i = 0; i < n; i++){
+                        inserir_abb(rand() % 100, &abb);
+                    }
+                    break;}
+            case 2: {int valor;
+                    printf("Digite o valor: ");
+                    scanf("%d", &valor);
+                    inserir_abb(valor, &abb);
+                    break;}
+            case 3: exibir_abb(&abb);
+                    break;
+            case 4: {int x;
+                    printf("Digite o valor para buscar: ");
+                    scanf("%d", &x);
+                    if(busca_abb(x, &abb)){
+                        printf("%d esta na arvore!\n", x);
+                    }
+                    else{
+                        printf("%d nao esta na arvore!\n", x);
+                    }
+                    break;}
+            case 0: printf("Saindo...\n");
+                    break;
+            default: printf("Isso nao eh uma opcao!\n");
+                     break;
+        }
+    }while(op != 0);
 
     return 0;
 }
